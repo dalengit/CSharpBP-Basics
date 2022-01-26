6f0834c96a92ec75c8bf09846edc8fd374181b3c
Dalen Chan <Dalen.Chan@brighthr.com>
2022-01-26 11:46:03 +0000

    Created class/adding unit test

---
+++ AcmeApp/Acme.Biz/Product.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Acme.Biz
+{
+    public class Product
+    {
+        private string _productName;
+
+        public string ProductName
+        {
+            get { return _productName; }
+            set { _productName = value; }
+        }
+
+        private string _description;
+
+        public  string Description
+        {
+            get { return _description; }
+            set { _description = value; }
+        }
+
+        private int _productId;
+
+        public int ProductId
+        {
+            get { return _productId; }
+            set { _productId = value; }
+        }
+
+        public string SayHello()
+        {
+
+        }
+    }
+}
