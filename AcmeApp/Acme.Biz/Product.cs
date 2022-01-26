64effecac9c87643326d13391364cb5969d058c2
Dalen Chan <Dalen.Chan@brighthr.com>
2022-01-26 13:14:07 +0000

    Added parameterized ctor

--- AcmeApp/Acme.Biz/Product.cs
+++ AcmeApp/Acme.Biz/Product.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,6 +9,20 @@ namespace Acme.Biz
 {
     public class Product
     {
+        // Ctor
+        public Product()
+        {
+            Console.WriteLine("Product instance created");
+        }
+
+        public Product(int productId, string productName, string description) : this()
+        {
+            this.ProductId = productId;
+            this.ProductName = productName;
+            this.Description = description;
+            Console.WriteLine("Product instance has a name: " + productName);
+        }
+
         public string ProductName { get; set; }
         public string Description { get; set; }
         public int ProductId { get; set; }
